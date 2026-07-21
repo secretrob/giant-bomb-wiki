b49e8ad5f40a092eed524d118008f6bbb92eea10
secretrob <magus91@gmail.com>
2026-07-21 16:24:40 -0500

    Fix rm for xmls

--- data/initial_import_data_xmls/import.sh
+++
@@ -1,6 +0,0 @@
-for file in *.xml; do
-    echo "Importing $file into Docker container..."
-    php /var/www/html/maintenance/run.php /var/www/html/maintenance/importDump.php < "$file"
-done
-
-php /var/www/html/maintenance/run.php maintenance/rebuildrecentchanges.php
--- setup.sh
+++ setup.sh
@@ -101,7 +101,7 @@ fi
 docker cp installinitialwikidata.sh $WIKI_CONTAINER:/installinitialwikidata.sh
 docker exec $WIKI_CONTAINER chmod 755 /installinitialwikidata.sh
 docker exec $WIKI_CONTAINER /bin/bash /installinitialwikidata.sh
-docker exec $WIKI_CONTAINER rm -rf /data/initial_import_data_xmls/
+docker exec -it $WIKI_CONTAINER rm -rf /data/initial_import_data_xmls
 echo "✓ Initial data backup installed"
 
 # Get stats
